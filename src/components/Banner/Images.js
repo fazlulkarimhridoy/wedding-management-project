054485d1ab259dbe130978afc6a67d5eb25385b3
Fazlul Karim Hridoy <fkhridoy4321@gmail.com>
2023-12-09 05:48:16 +0600

    Update Images.js

--- src/components/Banner/Images.js
+++ src/components/Banner/Images.js
@@ -1,1 +1,1 @@
-export const BannerImage = "https://i.ibb.co/SmnZfNf/Sidharth-Malhotra-Kiara-Advani-Wedding-First-photos-of-Shershaah-couple-are-etheteral-Ab-humari-perm.jpg"
+// export const BannerImage = "https://i.ibb.co/SmnZfNf/Sidharth-Malhotra-Kiara-Advani-Wedding-First-photos-of-Shershaah-couple-are-etheteral-Ab-humari-perm.jpg"
